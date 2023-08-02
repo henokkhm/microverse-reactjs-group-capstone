3013e61d616cb626baa733dd0bad2f8532f090ff
Henok Kirubel <henokhm2@gmail.com>
2023-08-02 19:53:52 +0300

    Display rockets data in rockets page (unstyled)

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,8 +1,13 @@
+import { useSelector } from 'react-redux';
+
 function Rockets() {
+  const { rocketsList } = useSelector((store) => store.rockets);
+
   return (
     <section>
       <h2>Rockets</h2>
       <div>Rockets page content</div>
+      <pre>{JSON.stringify(rocketsList, null, 2)}</pre>
     </section>
   );
 }
