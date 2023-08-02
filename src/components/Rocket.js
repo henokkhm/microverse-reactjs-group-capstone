91e7e935830df86bdbaaea2545ce283f3df3c313
Henok Kirubel <henokhm2@gmail.com>
2023-08-02 23:20:30 +0300

    Add Rocket component styles

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,13 +1,19 @@
 import PropTypes from 'prop-types';
 
+import styles from '../styles/Rocket.module.css';
+
 function Rocket({ name, description, imgUrl }) {
   return (
-    <div>
-      <img src={imgUrl} alt={`${name} rocket`} />
-      <div>
+    <div className={styles.rocketCard}>
+      <div className={styles.rocketImgWrapper}>
+        <img className={styles.rocketImg} src={imgUrl} alt={`${name} rocket`} />
+      </div>
+      <div className={styles.rocketCardBody}>
         <h3 className={styles.rocketName}>{name}</h3>
-        <p>{description}</p>
-        <button type="button">Reserve Rocket</button>
+        <p className={styles.rocketDescription}>{description}</p>
+        <button className={styles.reserveRocketBtn} type="button">
+          Reserve Rocket
+        </button>
       </div>
     </div>
   );
