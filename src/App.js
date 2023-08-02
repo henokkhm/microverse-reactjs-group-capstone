6c0fefdf56bd35ef42c1a5b0efc8eb9d5e27e480
Henok Kirubel <henokhm2@gmail.com>
2023-08-02 10:49:04 +0300

    Add homepage and my-profile routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+
 import styles from './styles/App.module.css';
 import Header from './components/Header';
+import Home from './pages/Home';
+import MyProfile from './pages/MyProfile';
 
 function App() {
   return (
     <div className={styles.app}>
       <Header />
       <main>
-        Main Page Content
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="my-profile" element={<MyProfile />} />
+        </Routes>
       </main>
     </div>
   );
