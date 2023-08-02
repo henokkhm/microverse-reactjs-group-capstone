2da70d12d69d95c904175379ea85c7e22cce857d
Henok Kirubel <henokhm2@gmail.com>
2023-08-02 10:00:22 +0300

    Add homepage and my-profile page

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <section>
+      <h2>Home</h2>
+      <div>Homepage content</div>
+    </section>
+  );
+}
+
+export default Home;
---
+++ src/pages/MyProfile.js
@@ -0,0 +1,10 @@
+function MyProfile() {
+  return (
+    <section>
+      <h2>My Profile</h2>
+      <div>MyProfile page content</div>
+    </section>
+  );
+}
+
+export default MyProfile;
