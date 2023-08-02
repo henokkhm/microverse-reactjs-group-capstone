3201ef13f240560645b0b04d90ea3b561bfa1ccd
Henok Kirubel <henokhm2@gmail.com>
2023-08-02 11:29:09 +0300

    Add Navbar component styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,21 @@
 import { NavLink } from 'react-router-dom'; // If you're using React Router for navigation
 
+import styles from '../styles/Navbar.module.css';
+
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to="/">
+    <nav className={styles.navigation}>
+      <ul className={styles.navList}>
+        <li className={styles.navListItem}>
+          <NavLink className={styles.navLink} to="/">
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink to="/my-profile">
+          <NavLink
+            className={`${styles.navLink} ${styles.myProfileLink}`}
+            to="/my-profile"
+          >
             My Profile
           </NavLink>
         </li>
