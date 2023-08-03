492cf8fbfe940c1825834aca466be051f79f97f6
Henok Kirubel <henokhm2@gmail.com>
2023-08-04 00:05:32 +0300

    Add reserved rockets to MyProfile page

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -7,9 +7,13 @@ function MyProfile() {
   const { mission } = useSelector(selectsAllMissions);
   const joinMission = mission.filter((mission) => mission.reservation === true);
 
+  const { rocketsList } = useSelector((state) => state.rockets);
+  const reservedRockets = rocketsList.filter((rocket) => rocket.isReserved);
+
   return (
     <section className={styles.profiler}>
       <JoinedMissionsList data={joinMission} />
+      <pre>{JSON.stringify(reservedRockets, null, 2)}</pre>
     </section>
   );
 }
