1e4155d47648a85b18fa01f9d90c4b8aae7f1fca
clarence <tegumclarence@gmail.com>
2023-08-03 11:16:30 +0100

    Added slection button to join mission and added to my profile

--- src/profile/Missions.jsx
+++ src/profile/Missions.jsx
@@ -1,12 +1,11 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
-// import styles from './Mission.module.css';
-import styles from '../Profiless.module.css';
+import styles from '../components/Profiless.module.css';
 
 function MyMissions({ data }) {
   return (
     <table className="profiler-table">
-      {(data.length) < 0 ? <th>My Missions</th> : <th>My Missions</th> }
+      {data.length < 0 ? <th>My Missions</th> : <th>My Missions</th>}
       {data.map((mission) => (
         <tr key={mission.id} className={styles.profilertable}>
           <td className={styles.tabledata}>{mission.mission_name}</td>
@@ -14,14 +13,6 @@ function MyMissions({ data }) {
       ))}
     </table>
 
-  // <div className={styles.ourMission}>
-  //   <h2>My Missions</h2>
-  //   <ul>
-  //     {data.map((mission) => (
-  //       <li key={mission.id}>{mission.mission_name}</li>
-  //     ))}
-  //   </ul>
-  // </div>
   );
 }
 MyMissions.propTypes = { data: PropTypes.arrayOf(PropTypes.string).isRequired };
