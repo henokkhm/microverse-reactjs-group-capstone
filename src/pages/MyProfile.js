de8d68b1bba91f3971b00da85043bf731ac5ae4a
Henok Kirubel <henokhm2@gmail.com>
2023-08-04 00:26:18 +0300

    Use GenericList component for lists of joined missions and reserved rockets

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,19 +1,40 @@
 import { useSelector } from 'react-redux';
-import JoinedMissionsList from '../components/JoinedMissionsList';
+import GenericList from '../components/GenericList';
 import styles from '../styles/MyProfile.module.css';
 import { selectsAllMissions } from '../redux/mission/misionSlice';
 
 function MyProfile() {
   const { mission } = useSelector(selectsAllMissions);
-  const joinMission = mission.filter((mission) => mission.reservation === true);
+  const joinedMissions = mission
+    .filter((mission) => mission.reservation === true)
+    .map((missionData) => ({
+      id: missionData.mission_id,
+      value: missionData.mission_name,
+    }));
 
   const { rocketsList } = useSelector((state) => state.rockets);
-  const reservedRockets = rocketsList.filter((rocket) => rocket.isReserved);
+  const reservedRockets = rocketsList
+    .filter((rocket) => rocket.isReserved)
+    .map(({ id, name }) => ({ id, value: name }));
 
   return (
     <section className={styles.profiler}>
-      <JoinedMissionsList data={joinMission} />
-      <pre>{JSON.stringify(reservedRockets, null, 2)}</pre>
+      <div>
+        <h3>My Missions</h3>
+        {joinedMissions.length ? (
+          <GenericList data={joinedMissions} />
+        ) : (
+          <p>You have not joined any missions.</p>
+        )}
+      </div>
+      <div>
+        <h3>My Rockets</h3>
+        {reservedRockets.length ? (
+          <GenericList data={reservedRockets} />
+        ) : (
+          <p>You have not reserved any rockets.</p>
+        )}
+      </div>
     </section>
   );
 }
